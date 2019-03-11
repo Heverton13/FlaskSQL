9cb1374ded8b9f1a2b3f03d539a05d892d3587cd
Heverton13 <heverton.gomes2018@gmail.com>
2019-03-11 10:54:40 -0300

    Atualização no ADD e mudança no models

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -2,8 +2,7 @@ import os
 
 from app import app
 from app import db
-from flask import render_template, request, redirect
-
+from flask import render_template, request, redirect, abort
 from flask_sqlalchemy import SQLAlchemy
 from app.models.tables import Pessoa
 
@@ -11,10 +10,21 @@ from app.models.tables import Pessoa
 @app.route("/", methods=["GET","POST"])
 def index():
     if request.form:
-        pessoa = Pessoa(name=request.form.get("nome"), 
-                cpf=request.form.get("cpf"),
-                idade=request.form.get("idade"))
-        db.session.add(pessoa)
-        db.session.commit()
+        try:         
+                pessoa = Pessoa(name=request.form.get("nome"), 
+                        cpf=request.form.get("cpf"),
+                        idade=request.form.get("idade"))
+                db.session.add(pessoa)
+                db.session.commit()
+        except:
+                abort(404)
     pessoas = Pessoa.query.all()
     return render_template('index.html', pessoas=pessoas)
+
+""" @app.route("/delete", methods=["POST"])
+def """
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('error.html'), 404
+
--- app/models/tables.py
+++ app/models/tables.py
@@ -3,9 +3,8 @@ from app import db
 class Pessoa(db.Model):
     __tablename__="pessoa"
 
-    id = db.Column(db.Integer, primary_key=True)
+    cpf = db.Column(db.String(11), nullable=True,primary_key=True)
     name = db.Column(db.String(80), nullable=True)
-    cpf = db.Column(db.String(11), nullable=True)
     idade = db.Column(db.String(11), nullable=True)
 
     # Aqui eu poderia fazer o def __init__ mudaria somente a ordem que ficaria igual a do def __init__
